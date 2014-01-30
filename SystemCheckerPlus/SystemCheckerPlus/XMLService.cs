4a7789f0e8acd61eabcf05ec6f6f6a9ef9a308cd
Cameron VanHouzen <vanhouc@mail.nmc.edu>
2014-01-30 11:52:50 -0500

    Retargeted back to 3.5

--- SystemCheckerPlus/SystemCheckerPlus/XMLService.cs
+++ SystemCheckerPlus/SystemCheckerPlus/XMLService.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.IO;
+using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
 namespace SystemCheckerPlus
 {
     class XMLService
