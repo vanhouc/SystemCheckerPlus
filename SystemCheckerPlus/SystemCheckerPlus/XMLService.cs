39faa69675f07a0b94b7df92b81f966b2eef2b80
Cameron VanHouzen <vanhouc@mail.nmc.edu>
2014-01-30 11:53:16 -0500

    Removed 4.5.1 assemblies

--- SystemCheckerPlus/SystemCheckerPlus/XMLService.cs
+++ SystemCheckerPlus/SystemCheckerPlus/XMLService.cs
@@ -3,8 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.IO;
-using System.Threading.Tasks;
-using System.Runtime.CompilerServices;
 namespace SystemCheckerPlus
 {
     class XMLService
