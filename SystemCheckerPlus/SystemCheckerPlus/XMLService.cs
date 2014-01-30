46810d6361213c3e898c7be74192c5b72a82b334
Cameron VanHouzen <vanhouc@mail.nmc.edu>
2014-01-30 11:25:05 -0500

    Added XMLService class
    Added prop and constructor

---
+++ SystemCheckerPlus/SystemCheckerPlus/XMLService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+namespace SystemCheckerPlus
+{
+    class XMLService
+    {
+        private string _xmlSource;
+
+        public XMLService(string xmlSource)
+        {
+            if (File.Exists(xmlSource))
+            {
+                _xmlSource = xmlSource;
+            }
+            else
+            {
+                throw new FileNotFoundException(String.Format("Could not find file: {0}", xmlSource));
+            }
+        }
+    }
+}
