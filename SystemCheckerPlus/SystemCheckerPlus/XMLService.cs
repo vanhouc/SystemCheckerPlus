235358b00556ec48ba3c55c8cb573973cfa6daf5
Cameron VanHouzen <vanhouc@mail.nmc.edu>
2014-01-30 12:07:31 -0500

    XMLService stores internal XDocument but no file string

--- SystemCheckerPlus/SystemCheckerPlus/XMLService.cs
+++ SystemCheckerPlus/SystemCheckerPlus/XMLService.cs
@@ -3,17 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.IO;
+using System.Xml.Linq;
 namespace SystemCheckerPlus
 {
     class XMLService
     {
-        private string _xmlSource;
-
+        private XDocument doc;
         public XMLService(string xmlSource)
         {
             if (File.Exists(xmlSource))
             {
-                _xmlSource = xmlSource;
+                doc = XDocument.Load(xmlSource);
             }
             else
             {
