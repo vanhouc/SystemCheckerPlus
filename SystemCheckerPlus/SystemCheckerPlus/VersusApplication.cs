ff57e57822bd411fc29686d88daa658d644c2d04
Cameron VanHouzen <vanhouc@mail.nmc.edu>
2014-01-30 11:13:09 -0500

    Added VersusApplication Added Basic Properties and Default Constructor

---
+++ SystemCheckerPlus/SystemCheckerPlus/VersusApplication.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SystemCheckerPlus
+{
+    class VersusApplication
+    {
+        private string _displayName;
+
+        public string DisplayName
+        {
+            get { return _displayName; }
+            set { _displayName = value; }
+        }
+
+        private string _vDataName;
+
+        public string VDataName
+        {
+            get { return _vDataName; }
+            set { _vDataName = value; }
+        }
+
+        private string[] _bupFiles;
+
+        public string[] BUPFiles
+        {
+            get { return _bupFiles; }
+            set { _bupFiles = value; }
+        }
+
+        private string _appFolder;
+
+        public string AppFolder
+        {
+            get { return _appFolder; }
+            set { _appFolder = value; }
+        }
+        
+        public VersusApplication()
+        {
+            DisplayName = "Test Application";
+            VDataName = null;
+            BUPFiles = null;
+            AppFolder = null;
+        }
+    }
+}
