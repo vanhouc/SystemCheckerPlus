c4af36a5b5d7c2a3ece03e32ac085a9e380f93bf
Cameron VanHouzen <vanhouc@mail.nmc.edu>
2014-01-31 10:28:11 -0500

    Added AppController class
    This will handle working with Application data

---
+++ SystemCheckerPlus/SystemCheckerPlus/VersusAppController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SystemCheckerPlus
+{
+    class VersusAppController
+    {
+        private List<VersusApplication> _appList;
+
+        public List<VersusApplication> AppList
+        {
+            get { return _appList; }
+            set { _appList = value; }
+        }
+
+        public VersusAppController()
+        {
+            AppList = new List<VersusApplication>();
+        }
+        public VersusAppController(VersusApplication[] appList)
+        {
+            AppList = appList;
+        }
+    }
+}
