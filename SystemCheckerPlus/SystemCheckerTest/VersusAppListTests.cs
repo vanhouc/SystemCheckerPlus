0881ac221f957bb3a36c216c63bb2e70656f14b4
Cameron VanHouzen <vanhouc@mail.nmc.edu>
2014-01-31 12:34:35 -0500

    Added Test Solution

---
+++ SystemCheckerPlus/SystemCheckerTest/VersusAppListTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SystemCheckerPlus;
+using System.Diagnostics;
+
+namespace SystemCheckerTest
+{
+    [TestClass]
+    public class VersusAppListTests
+    {
+        [TestMethod]
+        public void ConstructorEmptyArray()
+        {
+            VersusApplication[] testArray = new VersusApplication[0];
+            VersusAppList testController = new VersusAppList(testArray);
+            CollectionAssert.AreEqual(new string[] { "No Applications Present" }, testController.GetAllProp("DisplayName"));
+        }
+        [TestMethod]
+        public void GetAllPropDN()
+        {
+            VersusApplication[] testArray = { new VersusApplication(), new VersusApplication("Just Checking"), new VersusApplication() };
+            VersusAppList testController = new VersusAppList(testArray);
+            CollectionAssert.AreEqual(new string[] { "Test Application", "Just Checking", "Test Application" }, testController.GetAllProp("DisplayName"));
+        }
+        [TestMethod]
+        public void GetElement()
+        {
+            VersusApplication[] testArray = { new VersusApplication(), new VersusApplication("Just Checking"), new VersusApplication() };
+            VersusAppList testController = new VersusAppList(testArray);
+            Assert.AreEqual(testArray[1], testController.GetElement(typeof(VersusApplication), "DisplayName", "Just Checking")[0]);
+        }
+    }
+}
---
+++ SystemCheckerPlus/SystemCheckerTest/XMLServiceTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SystemCheckerPlus;
+using System.Xml.Linq;
+
+namespace SystemCheckerTest
+{
+    [TestClass]
+    public class XMLServiceTests
+    {
+        [TestMethod]
+        public void GetCorrectElement()
+        {
+            XMLService elementTester = new XMLService(new MockXML());
+            Assert.AreEqual("Bar is maximum", elementTester.GetElementValue(new string[] { "Applications", "Foo", "FooBar" }));
+        }
+    }
+    public class MockXML : IXDocProvider
+    {
+        private XDocument _doc;
+        public MockXML()
+        {
+            _doc = new XDocument(
+                new XElement("Applications",
+                    new XElement("Foo",
+                        new XElement("FooBar", "Bar is maximum")
+                        )
+                    )
+                );
+        }
+        public XDocument Doc
+        {
+            get
+            {
+                return _doc;
+            }
+        }
+    }
+}
