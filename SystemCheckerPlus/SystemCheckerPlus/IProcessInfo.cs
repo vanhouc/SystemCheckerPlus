4459e2ead30a0d7776625cb74bccdda2666b3bc8
Cameron VanHouzen <vanhouc@mail.nmc.edu>
2014-01-31 14:01:54 -0500

    Added IProcessInfo interface
    This interface will be used to inject a performance data object

---
+++ SystemCheckerPlus/SystemCheckerPlus/IProcessInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SystemCheckerPlus
+{
+    interface IProcessInfo
+    {
+        public float TotalCPUUsage();
+
+        public float ProcessCPUUsage(string processName);
+
+        public float ProcessPrivateMemory(string processName);
+    }
+}
