c00525b9566cd7dc628b40362e174550a786aa42
Cameron VanHouzen <vanhouc@mail.nmc.edu>
2014-01-31 12:34:08 -0500

    Changed Name of VersusAppController
    Also added accessor methods

---
+++ SystemCheckerPlus/SystemCheckerPlus/VersusAppList.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+
+namespace SystemCheckerPlus
+{
+    public class VersusAppList
+    {
+        private VersusApplication[] _appList;
+
+        public VersusAppList(VersusApplication[] appList)
+        {
+            if (appList.Length < 1)
+                _appList = new VersusApplication[] { new VersusApplication("No Applications Present") };
+            else
+            _appList = appList;
+        }
+        public string[] GetAllProp(string propertyName)
+        {
+            PropertyInfo objProp = typeof(VersusApplication).GetProperty(propertyName);
+            return _appList.Select(x => objProp.GetValue(x, null) as string).ToArray();
+        }
+        public VersusApplication[] GetElement(Type objType, string refProp, string refValue)
+        {
+            PropertyInfo objProp = typeof(VersusApplication).GetProperty(refProp);
+            return _appList.Where(x => (string)objProp.GetValue(x, null) == refValue).ToArray();
+        }
+    }
+}
