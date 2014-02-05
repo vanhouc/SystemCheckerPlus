8d0c4923f9f3d350ff81401b5d5c2dc9a8548000
Cameron VanHouzen <vanhouc@mail.nmc.edu>
2014-02-04 20:28:19 -0500

    Added ProcessChecker

---
+++ SystemCheckerPlus/SystemCheckerPlus/ProcessChecker.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+using System.Threading;
+using System.ComponentModel;
+using System.Timers;
+
+namespace SystemCheckerPlus
+{
+    class ProcessChecker : IProcessInfo
+    {
+        private Mutex mut = new Mutex();
+        private float _cpuUsage;
+
+        public float CPUUSAGE
+        {
+            get { return _cpuUsage; }
+            set
+            {
+                _cpuUsage = value;
+            }
+        }
+
+        System.Timers.Timer pollTimer;
+        public ProcessChecker()
+        {
+            pollTimer = new System.Timers.Timer(1000);
+            pollTimer.Elapsed += new ElapsedEventHandler(QueuePoll);
+        }
+        public void QueuePoll(object source, ElapsedEventArgs e)
+        {
+            ThreadPool.QueueUserWorkItem(new WaitCallback(GetCPUUSage));
+        }
+        public void GetCPUUSage(object stateInfo)
+        {
+            PerformanceCounter cpuCounter = new PerformanceCounter();
+            cpuCounter.CategoryName = "Processor";
+            cpuCounter.CounterName = "% Processor Time";
+            cpuCounter.InstanceName = "_Total";
+            cpuCounter.NextValue();
+            Thread.Sleep(1000);
+            cpuCounter.NextValue();
+        }
+    }
+}
