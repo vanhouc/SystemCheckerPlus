fba78070428fdcc038cb024b4e6a73f469b07631
Cameron VanHouzen <vanhouc@mail.nmc.edu>
2014-03-06 17:04:23 -0500

    Initial Imp of MWVM

--- SystemCheckerPlus/SystemCheckerPlus/MainWindowViewModel.cs
+++ SystemCheckerPlus/SystemCheckerPlus/MainWindowViewModel.cs
@@ -2,15 +2,39 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Win32;
+using System.IO;
 
 namespace SystemCheckerPlus
 {
-    class MainWindowViewModel
+    class MainWindowViewModel : ObservableObject
     {
+        XMLService xmlService;
+        private AppService _appList;
 
+        public AppService AppList
+        {
+            get { return _appList; }
+            set 
+            {
+                _appList = value;
+                RaisePropertyChanged("AppList");
+            }
+        }
+        
         public MainWindowViewModel()
         {
 
+        }
+        public void LoadVersusAppData()
+        {
+            OpenFileDialog openVData = new OpenFileDialog();
+            openVData.Filter = "Extensible Markup Language *.xml|*.xml";
+            openVData.ShowDialog();
+            if (openVData.FileName != null && File.Exists(openVData.FileName))
+            {
+                xmlService = new XMLService(new XMLLoader(openVData.FileName));
+            }
         }
     }
 }
