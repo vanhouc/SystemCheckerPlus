f1b3c47e972a34b42f4d757cda54a1ae8726e67c
Cameron VanHouzen <vanhouc@mail.nmc.edu>
2014-03-06 16:23:59 -0500

    Added Tests for ProcessService

---
+++ SystemCheckerPlus/SystemCheckerTest/ProcessServiceTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SystemCheckerPlus;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace SystemCheckerTest
+{
+    [TestClass]
+    public class ProcessServiceTest
+    {
+        [TestMethod]
+        public void TestAsyncPoll()
+        {
+            ProcessService testPService = new ProcessService();
+            float cpu = testPService.TotalCPUAsync().Result;
+            Assert.IsNotNull(cpu);
+        }
+        [TestMethod]
+        public void TestMultiplePolls()
+        {
+            ProcessService testPService = new ProcessService();
+            float[] testResults = new float[3];
+            for (int i = 0; i < 3; i++)
+            {
+                testResults[i] = testPService.TotalCPUAsync().Result;
+            }
+            CollectionAssert.AllItemsAreNotNull(testResults);
+        }
+        [TestMethod]
+        public void TestSynchronousPoll()
+        {
+            ProcessService testPService = new ProcessService();
+            float cpu = testPService.TotalCPUAsync().Result;
+            Assert.IsNotNull(cpu);
+        }
+        [TestMethod]
+        public void TestProcessPoll()
+        {
+            ProcessService testPService = new ProcessService();
+            Process thisProc = Process.GetCurrentProcess();
+            float cpuProc = testPService.ProcessCPUAsync(thisProc.ProcessName).Result;
+            Assert.IsNotNull(cpuProc);
+        }
+    }
+}
