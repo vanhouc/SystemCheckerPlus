3653e2bacfd66ebab7e0b9d5f6c62e7218e8ae8e
Cameron VanHouzen <vanhouc@mail.nmc.edu>
2014-03-07 15:18:56 -0500

    Added Performance Properties, Notify handlers

--- SystemCheckerPlus/SystemCheckerPlus/Application.cs
+++ SystemCheckerPlus/SystemCheckerPlus/Application.cs
@@ -2,25 +2,34 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Xml.Linq;
 
 namespace SystemCheckerPlus
 {
-    public class Application
+    public class Application : ObservableObject
     {
         private string _displayName;
 
         public string DisplayName
         {
             get { return _displayName; }
-            set { _displayName = value; }
+            set 
+            {
+                _displayName = value;
+                RaisePropertyChanged("DisplayName");
+            }
         }
 
-        private string _vDataName;
+        private string procName;
 
-        public string VDataName
+        public string ProcName
         {
-            get { return _vDataName; }
-            set { _vDataName = value; }
+            get { return procName; }
+            set 
+            {
+                procName = value;
+                RaisePropertyChanged("ProcName");
+            }
         }
 
         private string[] _bupFiles;
@@ -28,7 +37,11 @@ namespace SystemCheckerPlus
         public string[] BUPFiles
         {
             get { return _bupFiles; }
-            set { _bupFiles = value; }
+            set 
+            {
+                _bupFiles = value;
+                RaisePropertyChanged("BUPFiles");
+            }
         }
 
         private string _appFolder;
@@ -36,7 +49,11 @@ namespace SystemCheckerPlus
         public string AppFolder
         {
             get { return _appFolder; }
-            set { _appFolder = value; }
+            set 
+            {
+                _appFolder = value;
+                RaisePropertyChanged("AppFolder");
+            }
         }
 
         private int _appVersion;
@@ -44,13 +61,73 @@ namespace SystemCheckerPlus
         public int AppVersion
         {
             get { return _appVersion; }
-            set { _appVersion = value; }
+            set 
+            {
+                _appVersion = value;
+                RaisePropertyChanged("AppVersion");
+            }
+        }
+        private Queue<float> _procUsage = new Queue<float>();
+
+        public float ProcUsage
+        {
+            get 
+            {
+                if (_procUsage.Count != 0)
+                    return _procUsage.Average();
+                else
+                    return 0;
+            }
+            set
+            {
+                if (_procUsage.Count > 30)
+                    _procUsage.Dequeue();
+                _procUsage.Enqueue(value);
+                RaisePropertyChanged("ProcUsage");
+            }
+        }
+
+        private float _memUsage = 0;
+
+        public float MemUsage
+        {
+            get { return _memUsage; }
+            set 
+            {
+                _memUsage = value;
+                RaisePropertyChanged("MemUsage");
+            }
+        }
+        private List<XDocument> _appDefFiles;
+
+        public List<XDocument> AppDefFiles
+        {
+            get { return _appDefFiles; }
+            set 
+            {
+                _appDefFiles = value;
+                RaisePropertyChanged("AppDefFiles");
+            }
+        }
+
+        private XDocument _appDefFile;
+
+        public XDocument AppDefFile
+        {
+            get { return _appDefFile; }
+            set 
+            {
+                _appDefFile = value;
+                RaisePropertyChanged("AppDefFile");
+            }
         }
         
+        
+
         public Application()
         {
             DisplayName = "Test Application";
-            VDataName = null;
+            ProcName = null;
             BUPFiles = null;
             AppFolder = null;
         }
