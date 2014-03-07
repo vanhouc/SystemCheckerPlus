bad32cb967637dbd1efc53632f1e5b8e3aa7e8d9
Cameron VanHouzen <vanhouc@mail.nmc.edu>
2014-03-07 15:22:49 -0500

    Added basic interface

--- SystemCheckerPlus/SystemCheckerPlus/MainWindowViewModel.cs
+++ SystemCheckerPlus/SystemCheckerPlus/MainWindowViewModel.cs
@@ -4,29 +4,50 @@ using System.Linq;
 using System.Text;
 using Microsoft.Win32;
 using System.IO;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Collections.ObjectModel;
 
 namespace SystemCheckerPlus
 {
-    class MainWindowViewModel : ObservableObject
+    public class MainWindowViewModel : ObservableObject
     {
-        XMLService xmlService;
-        private AppService _appList;
+        private float cpuUsage;
 
-        public AppService AppList
+        public float CPUUsage
         {
-            get { return _appList; }
-            set 
+            get { return cpuUsage; }
+            set
             {
-                _appList = value;
-                RaisePropertyChanged("AppList");
+                cpuUsage = value;
+                RaisePropertyChanged("CPUUsage");
             }
         }
-        
+
+        IXMLService xmlService;
+        Timer perfCounter;
+        private ObservableCollection<Application> _applications = new ObservableCollection<Application>();
+
+        public ObservableCollection<Application> Applications
+        {
+            get { return _applications; }
+            set
+            {
+                _applications = value;
+                RaisePropertyChanged("Applications");
+            }
+        }
+
         public MainWindowViewModel()
         {
-
+            perfCounter = new Timer(UpdatePerfCounters, null, 0, 1000);
+            Applications.Add(new Application
+            {
+                DisplayName = "Total CPU Usage",
+                ProcName = "Total CPU Usage"
+            });
         }
-        public void LoadVersusAppData()
+        public void LoadAppData()
         {
             OpenFileDialog openVData = new OpenFileDialog();
             openVData.Filter = "Extensible Markup Language *.xml|*.xml";
@@ -34,6 +55,38 @@ namespace SystemCheckerPlus
             if (openVData.FileName != null && File.Exists(openVData.FileName))
             {
                 xmlService = new XMLService(new XMLLoader(openVData.FileName));
+                InitializeAppData(xmlService);
+            }
+        }
+        public void InitializeAppData(IXMLService service)
+        {
+            if (service != null)
+            {
+                Applications = new ObservableCollection<Application>(service.GetAppData(new string[] { "Applications", "Application" }));
+            }
+        }
+        async private void UpdatePerfCounters(object state)
+        {
+            ProcessService procService = ProcessService.Instance;
+            CPUUsage = await procService.TotalCPUAsync();
+            if (Applications != null)
+            {
+                foreach (Application a in Applications)
+                {
+                    if (a.ProcName != null && a.ProcName != String.Empty)
+                    {
+                        if (a.ProcName == "Total CPU Usage")
+                        {
+                            a.ProcUsage = await procService.TotalCPUAsync();
+                            a.MemUsage = procService.AvailableMemory();
+                        }
+                        else
+                        {
+                            a.ProcUsage = await procService.ProcessCPUAsync(a.ProcName);
+                            a.MemUsage = procService.ProcessPrivateMemory(a.ProcName);
+                        }
+                    }
+                }
             }
         }
     }
---
+++ SystemCheckerPlus/SystemCheckerTest/MWVMTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SystemCheckerPlus;
+using System.Threading;
+using Moq;
+using System.Diagnostics;
+using System.Linq;
+
+namespace SystemCheckerTest
+{
+    [TestClass]
+    public class MWVMTest
+    {
+        [TestMethod]
+        public void PerfTimer()
+        {
+            MainWindowViewModel testMWVM = new MainWindowViewModel();
+            Thread.Sleep(2000);
+            float sample1 = testMWVM.CPUUsage;
+            Assert.IsTrue(testMWVM.CPUUsage != 0);
+            Thread.Sleep(1500);
+            float sample2 = testMWVM.CPUUsage;
+            Assert.AreNotEqual(sample1, sample2);
+        }
+    }
+}
