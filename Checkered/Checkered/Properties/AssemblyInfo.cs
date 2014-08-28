fca11c8a012433f4f363079b453d6e686c74e41a
vanhouc <vanhouc@mail.nmc.edu>
2014-08-28 14:17:37 -0400

    updated assembly to 0.1.1.2

--- Checkered/Checkered/Properties/AssemblyInfo.cs
+++ Checkered/Checkered/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Checkered")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Versus Technology Inc.")]
 [assembly: AssemblyProduct("Checkered")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.0.4")]
-[assembly: AssemblyFileVersion("0.1.0.4")]
+[assembly: AssemblyVersion("0.1.1.2")]
+[assembly: AssemblyFileVersion("0.1.1.2")]
